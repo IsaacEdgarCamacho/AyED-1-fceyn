24c1ae85cd4190871344865e1bd4f7a13140992a
camacho_isaac <isaaccamachoocampo@gmail.com>
2020-07-14 00:04:13 -0300

    agregando guias

--- Guia7/ej1/Test/puntajeTEST.cpp
+++ Guia7/ej1/Test/puntajeTEST.cpp
@@ -2,3 +2,30 @@
 #include "gtest/gtest.h"
 
 // Escribir tests aca:
+
+TEST(puntajeTest, NumeroMenor10) {
+    // setup
+    int n = 7;
+    //exercise
+    int result = puntaje(n);
+    // check
+    EXPECT_EQ(result,4);
+}
+
+TEST(puntajeTest, NumeroNegativo) {
+    // setup
+    int n = -6;
+    //exercise
+    int result = puntaje(n);
+    // check
+    EXPECT_EQ(result, -2 );
+}
+
+TEST(puntajeTest, CasoBorde) {
+    // setup
+    int n = 30;
+    //exercise
+    int result = puntaje(n);
+    // check
+    EXPECT_EQ(result, 40);
+}
--- Guia7/ej2/Test/llenarTaxisTEST.cpp
+++ Guia7/ej2/Test/llenarTaxisTEST.cpp
@@ -3,3 +3,33 @@
 
 // Escribir tests aca:
 
+//El test no pasa porque llenartaxis1 retorna 4 en lugar
+//de 3 de manera uqe no devuelve la minima cantidad de taxis
+    int g1 = 1,
+        g2 = 1,
+        g3 = 1;
+    int espect = 2;
+TEST(test1, llenarTaxis1) {
+    // setup
+    //exercise
+    int result = llenarTaxis1(g1,g2,g3);
+    // check
+    EXPECT_EQ(result,espect);
+}
+
+TEST(test2, llenarTaxis2) {
+    // setup
+    //exercise
+    int result = llenarTaxis2(g1,g2,g3);
+    // check
+    EXPECT_EQ(result,espect);
+}
+
+
+TEST(test3, llenarTaxis3) {
+    // setup
+    //exercise
+    int result = llenarTaxis3(g1,g2,g3);
+    // check
+    EXPECT_EQ(result,espect);
+}
